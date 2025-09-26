4ea1bd2721b061a82bad62b215ddaf674af54797
Laís Carvalho <88303945+LaysC@users.noreply.github.com>
2025-09-26 09:31:19 -0300

    Update and rename SearchGeneroMusicalResponse.java to SearchAcessorioResponse.java

--- src/main/java/org/acme/SearchAcessorioResponse.java
+++ src/main/java/org/acme/SearchAcessorioResponse.java
@@ -3,9 +3,9 @@ package org.acme;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SearchGeneroMusicalResponse {
-    public List<GeneroMusical> GenerosMusicais = new ArrayList<>();
-    public long TotalGenerosMusicais;
+public class SearchAcessorioResponse {
+    public List<Acessorio> Acessorios = new ArrayList<>();
+    public long TotalAcessorios;
     public int TotalPages;
     public boolean HasMore;
     public String NextPage;

3cd9c4d9a7fd2ec01fb294fbee3318728b709323
Laís Carvalho <88303945+LaysC@users.noreply.github.com>
2025-09-26 09:29:05 -0300

    Update and rename SearchArtistaResponse.java to SearchMarcaResponse.java

--- src/main/java/org/acme/SearchMarcaResponse.java
+++ src/main/java/org/acme/SearchMarcaResponse.java
@@ -3,9 +3,9 @@ package org.acme;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SearchArtistaResponse {
-    public List<Artista> Artistas = new ArrayList<>();
-    public long TotalArtistas;
+public class SearchMarcaResponse {
+    public List<Marca> Marcas = new ArrayList<>();
+    public long TotalMarcas;
     public int TotalPages;
     public boolean HasMore;
     public String NextPage;

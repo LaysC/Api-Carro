5b04711f713549478388b20b52d843f34e1c9a61
Laís Carvalho <88303945+LaysC@users.noreply.github.com>
2025-09-26 09:48:47 -0300

    Update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,22 +1,22 @@
 -- Insere dados básicos de teste para garantir a integridade do banco
 
 -- FichaMarca
-insert into FichaMarca (id, historia, fundadores, premiosConquistados) values (1, 'História da Ford.', 'Henry Ford', 'Carro do Ano da América do Norte');
-insert into FichaMarca (id, historia, fundadores, premiosConquistados) values (2, 'História da Tesla.', 'Elon Musk', 'Inovação da TIME Magazine');
+insert into FichaMarca (historia, fundadores, premiosConquistados) values ('História da Ford.', 'Henry Ford', 'Carro do Ano da América do Norte');
+insert into FichaMarca (historia, fundadores, premiosConquistados) values ('História da Tesla.', 'Elon Musk', 'Inovação da TIME Magazine');
 
 -- Marca
-insert into Marca (id, nomeDaMarca, paisDeOrigem, ficha_marca_id) values (1, 'Ford', 'Estados Unidos', 1);
-insert into Marca (id, nomeDaMarca, paisDeOrigem, ficha_marca_id) values (2, 'Tesla', 'Estados Unidos', 2);
+insert into Marca (nomeDaMarca, paisDeOrigem, ficha_marca_id) values ('Ford', 'Estados Unidos', 1);
+insert into Marca (nomeDaMarca, paisDeOrigem, ficha_marca_id) values ('Tesla', 'Estados Unidos', 2);
 
 -- Acessorio
-insert into Acessorio (id, nome, descricao) values (1, 'Ar Condicionado', 'Sistema de resfriamento.');
-insert into Acessorio (id, nome, descricao) values (2, 'GPS', 'Navegação por satélite.');
-insert into Acessorio (id, nome, descricao) values (3, 'Teto Solar', 'Vidro panorâmico.');
+insert into Acessorio (nome, descricao) values ('Ar Condicionado', 'Sistema de resfriamento.');
+insert into Acessorio (nome, descricao) values ('GPS', 'Navegação por satélite.');
+insert into Acessorio (nome, descricao) values ('Teto Solar', 'Vidro panorâmico.');
 
 -- Carro
-insert into Carro (id, modelo, anoFabricacao, marca_id, descricao, avaliacao, cilindradas) values (1, 'Mustang', 2023, 1, 'Carro esportivo lendário', 9.5, 5000);
-insert into Carro (id, modelo, anoFabricacao, marca_id, descricao, avaliacao, cilindradas) values (2, 'Fusion', 2020, 1, 'Sedan confortável e seguro.', 8.5, 2500);
-insert into Carro (id, modelo, anoFabricacao, marca_id, descricao, avaliacao, cilindradas) values (3, 'Model S', 2023, 2, 'Carro elétrico de luxo.', 9.9, 0);
+insert into Carro (modelo, anoFabricacao, marca_id, descricao, avaliacao, cilindradas) values ('Mustang', 2023, 1, 'Carro esportivo lendário', 9.5, 5000);
+insert into Carro (modelo, anoFabricacao, marca_id, descricao, avaliacao, cilindradas) values ('Fusion', 2020, 1, 'Sedan confortável e seguro.', 8.5, 2500);
+insert into Carro (modelo, anoFabricacao, marca_id, descricao, avaliacao, cilindradas) values ('Model S', 2023, 2, 'Carro elétrico de luxo.', 9.9, 0);
 
 -- Relacionamento carro_acessorio
 insert into carro_acessorio (carro_id, acessorio_id) values(1, 1);

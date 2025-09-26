c7818f9941dc1947cd454d4feee98854169c5748
Laís Carvalho <88303945+LaysC@users.noreply.github.com>
2025-09-26 10:06:59 -0300

    Update SearchFichaMarcaResponse.java

--- src/main/java/org/acme/SearchFichaMarcaResponse.java
+++ src/main/java/org/acme/SearchFichaMarcaResponse.java
@@ -3,9 +3,9 @@ package org.acme;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SearchFichaMarcaResponse {
-    public List<FichaMarca> FichasMarca = new ArrayList<>();
-    public long TotalFichasMarca;
+public class SearchMarcaResponse {
+    public List<Marca> Marcas = new ArrayList<>();
+    public long TotalMarcas;
     public int TotalPages;
     public boolean HasMore;
     public String NextPage;
